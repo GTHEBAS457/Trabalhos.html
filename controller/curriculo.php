7c18f03a5475016f58e3eac570fd0bffc6a314c1
GTHEBAS457 <silva.santos.guilherme0408@escola.pr.gov.br>
2025-07-29 09:22:39 -0300

    Update curriculo.php

--- controller/curriculo.php
+++ controller/curriculo.php
@@ -1,8 +1,49 @@
 <?php
-$nome = $_POST["Nome"];
-$telefone = $_POST["Telefone"];
-$email = $_POST["Email"];
-$endereço = $_POST["Endereço"];
+$nome = $_POST["nome"];
+$telefone = $_POST["telefone"];
+$email = $_POST["email"];
+$endereço = $_POST["endereço"];
 
+$efc = $_POST["efc"];
+$emc = $_POST["emc"];
+$esc = $_POST["esc"];
+$pgc = $_POST["pgc"];
+$mc = $_POST["mc"];
+
+$empresa = $_POST["empresa"];
+$data_de_admissão = $_POST["data_de_admissão"];
+$data_de_demissão = $_POST["data_de_demissão"];
+$atividade_exercida = $_POST["atividade_exercida"];
+
+$descrição = $_POST["descrição"];
+$horas = $_POST["horas"];
+$ano_de_conclusão = $_POST["ano_de_conclusão"];
+
+$informações = $_POST["informações"];
+
+echo "DADOS PESSOAIS";
+echo $telenone;
+echo $email;
+echo $endereço;
+
+echo "ESCOLARIDADE";
+echo $efc;
+echo $esc;
+echo $pgc;
+echo $mc;
+  
+echo "Experiencia Profissional";
+echo $empresa;
+echo $data_de_admissão;
+echo $data_de_demissão;
+echo $atividade_exercida;
+
+echo "Cursos e Idiomas";
+echo $descrição;
+echo $horas;
+echo $ano_de_conclusão;
+
+echo "INFORMAÇÔES";
+echo $informações;
 
 ?>
